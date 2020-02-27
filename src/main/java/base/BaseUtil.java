f88768a3f0d662e4b674710db420fa59a5e77cdf
Glenn <gadotey@outlook.com>
2020-02-27 11:06:17 -0500

    First Commit

---
+++ src/main/java/base/BaseUtil.java
@@ -0,0 +1,46 @@
+package base;
+
+import manager.DriverManager;
+import manager.DriverManagerFactory;
+import manager.DriverType;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+/**
+ * Created by Gadotey on 2/25/2020
+ */
+public class BaseUtil {
+    public WebDriver driver;
+    DriverManager driverManager;
+
+
+    @BeforeMethod
+    @Parameters("browserName")
+    public void setUp(@Optional String browserName) {
+        getLocalDriver(browserName);
+        //driver.manage().window().maximize();
+        driver.navigate().to("https://google.com");
+    }
+
+    public WebDriver getLocalDriver(String browserName) {
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            driverManager = DriverManagerFactory.getManager(DriverType.CHROME);
+            driver = driverManager.getDriver();
+        } else if (browserName.equalsIgnoreCase("Firefox")) {
+            driverManager = DriverManagerFactory.getManager(DriverType.FIREFOX);
+            driver = driverManager.getDriver();
+        }else {
+            //This exception will be displayed on the screen if the driver is not found.
+            throw new RuntimeException("No Such Driver Found!");
+        }
+        return driver;
+    }
+    @AfterMethod
+    public void tearDown() {
+        driverManager.quitDriver();
+    }
+
+}
